bdc803d5ec2fbe1fc5bf23001876fe9274b22590
cynwachi <christinenwachi@gmail.com>
2020-09-22 16:27:52 -0500

    debugging

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log('Add validation!');
+
+let form = document.querySelector("#parking-form")
+console.log(form);
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log("form was submitted");
+    const nameField = document.querySelector("#name")
+    console.log(nameField.value)
+    if (nameField.value.length > 0 )  {
+        nameField.parentElement.classList.add("input-invalid");
+        console.log(nameField.parentElement);
+    }
+})
